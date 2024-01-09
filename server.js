e6c52be6f2173b686d1acd135ee7d1857906563d
naripsaejan <naripsaejan@gmail.com>
2024-01-10 06:08:37 +0700

    update crud

--- server.js
+++ server.js
@@ -29,6 +29,27 @@ app.post("/products", async (req, res) => {
   }
 });
 
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.json(product);
+});
+
+app.put("/products/:id", async (req, res) => {
+  const payload = req.body;
+  const { id } = req.params;
+
+  const product = await Product.findByIdAndUpdate(id, { $set: payload });
+  res.json(product);
+});
+
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+
+  await Product.findByIdAndDelete(id);
+  res.status(204).end();
+});
+
 const server = app.listen(9000, () => {
   console.log("Application is running on port 9000");
 });
